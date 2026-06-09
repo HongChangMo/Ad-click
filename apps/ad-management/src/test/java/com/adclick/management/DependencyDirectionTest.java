05552616c7d1659277a855d0f4986a053f35862f
HongChangMo <hcm7402@naver.com>
2026-06-09 16:58:07 +0900

    test: verify ad-management does not depend on ad-click

---
+++ apps/ad-management/src/test/java/com/adclick/management/DependencyDirectionTest.java
@@ -0,0 +1,16 @@
+package com.adclick.management;
+
+import org.junit.jupiter.api.Test;
+
+class DependencyDirectionTest {
+
+    @Test
+    void adManagement_should_not_depend_on_adClick() {
+        try {
+            Class.forName("com.adclick.click.application.ClickFacadeService");
+            throw new AssertionError("ad-management must not depend on ad-click");
+        } catch (ClassNotFoundException e) {
+            // 정상: ad-click 클래스가 클래스패스에 없어야 함
+        }
+    }
+}
