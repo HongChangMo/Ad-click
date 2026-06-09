961c930bafcaae1a7f731e38d6dcd18195beeeb2
HongChangMo <hcm7402@naver.com>
2026-06-09 16:56:09 +0900

    chore: add ad-api module with Spring Boot application

---
+++ apps/ad-api/src/main/java/com/adclick/AdClickApplication.java
@@ -0,0 +1,11 @@
+package com.adclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdClickApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdClickApplication.class, args);
+    }
+}
---
+++ apps/ad-api/src/test/java/com/adclick/AdClickApplicationTest.java
@@ -0,0 +1,19 @@
+package com.adclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+    "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration"
+})
+class AdClickApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
